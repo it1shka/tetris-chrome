57bb4a58ce7bb96f5c806f62b25987f8071053d1
it1shka <2tishbel@gmail.com>
2023-07-30 17:38:50 +0200

    Added keyboard class

--- src/lib.ts
+++ src/lib.ts
@@ -1,1 +1,24 @@
 export type Mutable<T> = { -readonly [K in keyof T]: T[K] }
+
+export const Keyboard = new class {
+  private readonly actions: {[key: string]: Array<() => any>} = {}
+  constructor() {
+    window.addEventListener('keydown', (event) => {
+      const bucket = this.actions[event.key]
+      if (!bucket) return
+      event.preventDefault()
+      bucket.forEach(action => action())
+    })
+  }
+
+  // so far I need only subscription function
+
+  subscribe(action: () => any, ...keys: string[]) {
+    for (const key of keys) {
+      if (!this.actions[key]) this.actions[key] = []
+      this.actions[key].push(action)
+    }
+  }
+
+  //
+}
